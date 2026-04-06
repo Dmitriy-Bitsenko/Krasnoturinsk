1ed132c9538fa3574f5105fff1b22d44f1856151
Dmitriy-Bitsenko <bitsenko@inbox.ru>
2026-04-06 21:24:51 +0300

    small change

--- work_file.py
+++ work_file.py
@@ -7,15 +7,16 @@ class KrasnoturinskSpider(scrapy.Spider):
     start_urls = ["https://krasnoturinsk-ppi.ru/documents"]
 
     def parse(self, response):
-        documents = response.css("div.list-item a.caption")
-        for document in documents:
-            document_link = document.css("a.caption::attr(href)").get()
-            print(document_link)
-            yield scrapy.Request(url=document_link, callback=self.parse_all_documents)
+        n = 0
+        while n < 10000:
+            n += 1
+            document = f'http://krasnoturinsk-ppi.ru/documents/item/{n}'
+            print(document)
+            yield scrapy.Request(url=document, callback=self.parse_all_documents)
 
     def parse_all_documents(self, response):
         name = response.css('div.doc-item h2::text').get().replace('\n', '').replace('\r', '').lstrip().rstrip()
-        document_number= response.css('tr.info-item td.right::text').getall()[0]
+        document_number = response.css('tr.info-item td.right::text').getall()[0]
         accepted = response.css('tr.info-item td.right::text').getall()[1]
         published = response.css('tr.info-item td.right::text').getall()[2]
         source = response.css('tr.info-item td.right::text').getall()[3]
@@ -25,16 +26,8 @@ class KrasnoturinskSpider(scrapy.Spider):
         yield {'Наименование': name, 'Номер документа': document_number, 'Принят': accepted, 'Опубликован': published,
             'Источник': source, 'Принявший орган': adopting_agency, 'Номер опубликования': publication_number, 'Имя файла': file_name}
 
-        # next_page = response.css('div.pagination a').getall()
-        # if next_page is not None:
-        #     page = next_page
-        #     yield response.follow(page, callback=self.parse_all_documents)
-
-        n = 0
-        while n < 100000:
-            n += 1
-            #next_page = f'<a class="active">{n}</a>'
-            next_page = f'http://krasnoturinsk-ppi.ru/documents/item/{n}'
-            if next_page is not None:
-                page = next_page
-                yield response.follow(page, callback=self.parse_all_documents)
+#
+# next_page = f'http://krasnoturinsk-ppi.ru/documents/item/{n}'
+# if next_page is not None:
+#     page = next_page
+#     yield response.follow(page, callback=self.parse_all_documents)
